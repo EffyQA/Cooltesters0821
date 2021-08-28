c33349398d5796bf7081311503999c7ba8750014
Elfy <Elfy@DESKTOP-0I9FSC1>
2021-08-28 09:31:57 -0400

    first commit

---
+++ Selenium/src/test/java/NewTest.java
@@ -0,0 +1,13 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class NewTest {
+  @Test
+  public void f() {
+	  String  exePath="./Driver/chromedriver.exe";
+		System.setProperty("webdriver.chrome.driver", exePath);
+		WebDriver driver=new ChromeDriver();
+		driver.get("https://www.google.com");
+  }
+}
